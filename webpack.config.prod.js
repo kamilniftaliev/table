129692955cd78ecdcff1b3a52264e600f08b9e6a
Kamil Niftaliev <kamil.niftaliev@gmail.com>
2019-09-28 21:26:17 +0400

    s

--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -24,7 +24,7 @@ module.exports = {
     path: `${__dirname}/public`,
     filename: '[name].[hash].js',
     chunkFilename: '[name].[hash].js',
-    publicPath: '/',
+    publicPath: 'table/',
   },
 
   optimization: {
