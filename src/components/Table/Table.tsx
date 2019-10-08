fe01373b1f36f84d60b36522ee58217546b3c26d
Kamil Niftaliev <kamil.niftaliev@gmail.com>
2019-10-09 01:44:03 +0400

    Table | Added specific table fetch

--- src/components/Table/Table.tsx
+++ src/components/Table/Table.tsx
@@ -1,5 +1,8 @@
 import React from 'react';
 import styled from 'styled-components';
+import { useQuery } from 'react-apollo';
+
+import graph from '../../graph';
 
 interface Props {
   match: { params: { slug: string } };
@@ -12,7 +15,14 @@ function Table({
     params: { slug },
   },
 }: Props): React.ReactElement {
-  console.log('slug :', slug);
+  const { data, loading } = useQuery(graph.GetTable, { variables: { slug } });
+
+  if (loading) return <>Loading...</>;
+
+  const { table } = data;
+
+  console.log('table :', table);
+
   return <Container>AAAA</Container>;
 }
 
